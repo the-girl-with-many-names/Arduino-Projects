1f1f37e1e70ce1769e931ae534599995d568a65b
Alcifer Bijec <coolterminator222@gmail.com>
2019-03-19 14:57:49 -0400

    Update woo.ino

--- woo.ino
+++ woo.ino
@@ -15,17 +15,48 @@ void setup()
   sparki.moveLeft(90);
   sparki.moveBackward(6.5);
   sparki.beep(1000, 1000);
-
-  // Transfer to U posi.
+  
+  // transfer to U posi
+  sparki.transfer();
+  
+  // U
+  sparki.moveForward(11);
+  sparki.moveBackward(11);
   sparki.moveRight(90);
-  sparki.moveForward(8);
+  sparki.moveForward(5.5);
+  sparki.moveLeft(90);
+  sparki.moveForward(11);
+  sparki.moveBackward(11);
+  sparki.beep(1000, 1000);
+  
+  // Transfer to S posi.
+  sparki.transfer();
+  
+  // S
+  sparki.moveRight(90);
+  sparki.moveForward(5.5);
+  sparki.moveLeft(90);
+  sparki.moveForward(6.5);
+  sparki.moveLeft(90);
+  sparki.moveForward(5.5);
+  sparki.moveRight(90);
+  sparki.moveForward(4.5);
+  sparki.moveRight(90);
+  sparki.moveForward(5.5);
   sparki.beep(1000, 1000);
   sparki.moveLeft(90);
-  
-
+  sparki.moveBackward(11);
 }
 
 void loop()
 {
+}
+
+void transfer()
+{
+  sparki.moveRight(90);
+  sparki.moveForward(8);
+  sparki.beep(1000, 1000);
+  sparki.moveLeft(90);
 }
 
