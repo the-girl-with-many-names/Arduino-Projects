e7dee6f1ed4d579bd961e8044af644ec37a2ddf9
Alcifer Bijec <coolterminator222@gmail.com>
2019-03-19 08:44:00 -0400

    Add files via upload

---
+++ woo.ino
@@ -0,0 +1,31 @@
+#include <sparki.h>
+
+void setup()
+{
+  // F
+  sparki.moveForward(11);
+  sparki.moveRight(90);
+  sparki.moveForward(5.5);
+  sparki.moveBackward(5.5);
+  sparki.moveLeft(90);
+  sparki.moveBackward(4.5);
+  sparki.moveRight(90);
+  sparki.moveForward(5.5);
+  sparki.moveBackward(5.5);
+  sparki.moveLeft(90);
+  sparki.moveBackward(6.5);
+  sparki.beep(1000, 1000);
+
+  // Transfer to U posi.
+  sparki.moveRight(90);
+  sparki.moveForward(8);
+  sparki.beep(1000, 1000);
+  sparki.moveLeft(90);
+  
+
+}
+
+void loop()
+{
+}
+
